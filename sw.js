edbb56a0e3fa74a01f5aaf5b208e1ee5bab4ceba
croschel <croschel000@gmail.com>
2019-07-07 08:13:27 -0300

    More fixies

--- sw.js
+++ sw.js
@@ -5,11 +5,32 @@ self.addEventListener('install', function(event) {
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
-        'js/main.js',
-        'css/style.css',
-        'img/*.png',
-        "https://maps.googleapis.com/maps/api/js?key=AIzaSyDjkUnxuYPGTxhF5Zmbu65IN_8RbdRUg3Y&libraries=places&callback=initMap",
-        "https://maps.googleapis.com/maps/api/js?key=AIzaSyDjkUnxuYPGTxhF5Zmbu65IN_8RbdRUg3Y&libraries=places&callback=initMap"
+        '/index.html',
+        '/js/main.js',
+        '/js/dbhelper.js',
+        '/js/restaurant_info.js',
+        '/css/styles.css',
+        '/restaurant.html?id=1',
+        '/restaurant.html?id=2',
+        '/restaurant.html?id=3',
+        '/restaurant.html?id=4',
+        '/restaurant.html?id=5',
+        '/restaurant.html?id=6',
+        '/restaurant.html?id=7',
+        '/restaurant.html?id=8',
+        '/restaurant.html?id=9',
+        '/restaurant.html?id=10',
+        'img/1.png',
+        'img/2.png',
+        'img/3.png',
+        'img/4.png',
+        'img/5.png',
+        'img/6.png',
+        'img/7.png',
+        'img/8.png',
+        'img/9.png',
+        'img/10.png',
+        '/data/restaurants.json',
       ]);
     })
   );
@@ -34,9 +55,14 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
-    })
-  );
-});
+    }).catch(error => {
+      return new Response('Not connected to the internet', {
+        status: 404,
+        statusText: "Not connected to the internet"
+        });
+      })
+    );
+  });
 
 self.addEventListener('message', function(event) {
   if (event.data.action === 'skipWaiting') {
